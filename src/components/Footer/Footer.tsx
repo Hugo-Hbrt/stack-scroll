da72ae1b038ad24c051a35a49f7bb5b6267552e1
Hugo Hebert <hugo.hebert00@gmail.com>
2025-09-11 16:36:38 +0200

    [#17] Fix footer.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import GithubLogo from "@assets/images/github-mark.svg";
 const Footer = () => {
     // Add my github link
     return (
-        <footer className="w-full h-16 bg-gray-800 flex items-center justify-center text-white text-sm">
+        <footer className="w-full h-16 bg-gray-800 flex flex-wrap items-center justify-center text-white text-sm">
             &copy; 2025 StackScroll. All rights reserved.
             <a href="https://github.com/Hugo-Hbrt/stack-scroll" target="_blank" className="ml-2">
             <GithubLogo className="w-[24px] h-[24px] stroke-accent-950 fill-accent-950 inline mr-2"/>
