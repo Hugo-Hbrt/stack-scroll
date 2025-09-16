d309e88ca9010f1056519b50b4b69d06f44fd037
Hugo Hebert <hugo.hebert00@gmail.com>
2025-09-16 10:16:02 +0200

    Add Reddit config

---
+++ src/config/reddit.ts
@@ -0,0 +1,21 @@
+function assertIsString(variable: unknown): asserts variable is string {
+    
+    if (typeof variable !== "string")
+    {
+        throw new Error("Error during assertion")
+    }
+}
+
+assertIsString(import.meta.env.VITE_BASE_URL);
+assertIsString(import.meta.env.VITE_REDDIT_REDIRECT_ROUTE);
+
+const redirectUri = import.meta.env.VITE_BASE_URL + import.meta.env.VITE_REDDIT_REDIRECT_ROUTE;
+
+const REDDIT_CONFIG = {
+  clientId: import.meta.env.VITE_REDDIT_CLIENT_ID, // Public client ID
+  redirectUri: redirectUri,
+  scope: 'identity read submit vote history',
+  responseType: 'token', // Important: Use 'token' not 'code'
+};
+
+export default REDDIT_CONFIG;
