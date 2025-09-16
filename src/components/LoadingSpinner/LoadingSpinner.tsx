bb609a204c7ed8026e82e512d6344b6b31fa5694
Hugo Hebert <hugo.hebert00@gmail.com>
2025-09-16 10:20:14 +0200

    Add ariaLabel to LoadingSpinners's props.

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,12 +1,16 @@
 import AppLogo, { AppLogoSize } from "@components/AppLogo/AppLogo";
 
-const LoadingSpinner = () => {
+export interface LoadingSpinnerProps {
+  ariaLabel?: string;
+}
+
+const LoadingSpinner = ({ariaLabel} : LoadingSpinnerProps) => {
     return (<div
             data-testid="loading-spinner"
             className="flex justify-center my-4"
             role="status"
             aria-live="polite"
-            aria-label="Loading posts"
+            aria-label={ariaLabel}
           >
             <AppLogo size={AppLogoSize.Medium} className="animate-spin" />
           </div>);
