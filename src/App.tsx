233710b6bc80fce5b7c8926019d786780a5116da
Hugo Hebert <hugo.hebert00@gmail.com>
2025-09-16 10:38:04 +0200

    Add new pages to router.

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import FeedPage from "@pages/FeedPage";
 import Root from "@components/Root/Root";
 import ROUTES from "@config/routes";
 import AuthenticationPage from "@pages/AuthenticationPage";
+import LoginPage from "@pages/LoginPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Root />}>
-        <Route index element={<FeedPage/>} />
-        <Route path="post/:postId" element={<PostPage />}/>
-        <Route path={ROUTES.AUTH_REDIRECT} element={<AuthenticationPage />}/>
+        <Route index element={<LoginPage />} />
+        <Route path={ROUTES.FEED} element={<FeedPage />} />
+        <Route path="post/:postId" element={<PostPage />} />
+        <Route path={ROUTES.AUTH_REDIRECT} element={<AuthenticationPage />} />
       </Route>
     </Routes>
   );
